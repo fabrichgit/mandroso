cec71bcd4d8354efc3dca61958c76b5e96c20a2d
Fabrich Vohanson <hei.fabrich.2@gmail.com>
2025-02-17 08:02:32 +0300

    chore: status to dashbord

---
+++ src/pages/home/Gerant/Dashbord/Dashbord.tsx
@@ -0,0 +1,9 @@
+function Dashbord() {
+  return (
+    <div>
+      <h1>En attente de developement ...</h1>
+    </div>
+  )
+}
+
+export default Dashbord
--- src/pages/home/Gerant/HomeGerant.tsx
+++ src/pages/home/Gerant/HomeGerant.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import Layout from "../../../components/Layout"
 import Users from "./Users/Users"
+import Dashbord from "./Dashbord/Dashbord"
 
 function HomeGerant() {
   return (
     <Layout>
         <Routes>
-            <Route path="/" element/>
+            <Route path="/" element={<Dashbord/>}/>
             <Route path="/users" element={<Users/>}/>
         </Routes>
     </Layout>
