703cb2ff9e0c19abbee9c1a0289a5a78fd406280
Fabrich Vohanson <hei.fabrich.2@gmail.com>
2025-03-06 01:21:18 +0300

    styl: new layout bg

--- front/src/components/Layout.tsx
+++ front/src/components/Layout.tsx
@@ -2,18 +2,15 @@ import React from "react"
 import Sidebar from "./Sidebar"
 import { useParams } from "react-router-dom";
 
-function Layout({ children }: { children: React.ReactNode }) {
+function Layout({children}: {children: React.ReactNode}) {
 
-  const { tab } = useParams()
+  const {tab} = useParams()
 
   return (
-    <div className="flex w-full h-full ">
-      <Sidebar tab={tab} />
-      <main className="relative flex-grow w-full h-full overflow-y-auto bg-[url('/neon.png')] bg-cover">
-        <div className="absolute z-0 bg-white/90 w-full h-full"></div>
-        <div className="absolute z-10 w-full h-full">
-          {children}
-        </div>
+    <div className="flex w-full h-full">
+      <Sidebar tab={tab}/>
+      <main className="flex-grow w-full h-full overflow-y-auto bg-white">
+        {children}
       </main>
     </div>
   )
