6be2ac40b3bdc8d05aafc62f81c61a3ad9fa42ca
Fabrich Vohanson <hei.fabrich.2@gmail.com>
2025-03-22 15:37:12 +0300

    22-03-2025.3

--- front/src/pages/home/Gerant/Users/src/api/index.ts
+++ front/src/pages/home/Gerant/Users/src/api/index.ts
@@ -6,48 +6,77 @@ const API_URL = url(); // Replace with your actual API URL
 
 const api = axios.create({
   baseURL: API_URL,
-  headers: {
-    'Authorization': token(),
-  }
 });
 
 export const login = async (data: AuthData) => {
-  const response = await api.post('/user/auth', data);
+  const response = await api.post('/user/auth', data, {
+    headers: {
+      'Authorization': token(),
+    }
+  });
   return response.data;
 };
 
 export const getUsers = async () => {
-  const response = await api.get('/user/all');
+  const response = await api.get('/user/all',{
+    headers: {
+      'Authorization': token(),
+    }
+  });
   return response.data;
 };
 
 export const getUser = async (id: string) => {
-  const response = await api.get(`/user/${id}`);
+  const response = await api.get(`/user/${id}`,{
+    headers: {
+      'Authorization': token(),
+    }
+  });
   return response.data;
 };
 
 export const createUser = async (user: User) => {
-  const response = await api.post('/user/', user);
+  const response = await api.post('/user/', user,{
+    headers: {
+      'Authorization': token(),
+    }
+  });
   return response.data;
 };
 
 export const updateUser = async (user: User) => {
-  const response = await api.put('/user/', user);
+  const response = await api.put('/user/', user, {
+    headers: {
+      'Authorization': token(),
+    }
+  });
   return response.data;
 };
 
 export const deleteUser = async (id: string) => {
-  const response = await api.delete(`/user/${id}`);
+  const response = await api.delete(`/user/${id}`, {
+    headers: {
+      'Authorization': token(),
+    }
+  });
   return response.data;
 };
 
 export const getRoles = async () => {
-  const response = await api.get('/user/roles');
+  const response = await api.get('/user/roles', {
+    headers: {
+      'Authorization': token(),
+    }
+  });
   return response.data;
 };
 
 export const createRole = async (role: Role) => {
-  const response = await api.post('/user/role', role);
+  const response = await api.post('/user/role', role, {
+    headers: {
+      'Authorization': token(),
+    }
+  });
   return response.data;
 };
 
